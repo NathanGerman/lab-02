b77d90d6903f277ced89e3c18c93f6a191383220
Nathan <nathan.german.2029@heschel.org>
2026-01-29 13:26:50 -0500

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,10 @@
+import math
+
+def hypotenus(a, b):
+    return math.sqrt (a**2 + b**2)
+
+def num_digits (n):
+    return len(str(abs(n)))
+
+def tip_(total, percent):
+    return total * (perecent/100)
